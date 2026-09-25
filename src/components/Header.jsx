da4bfe7bd66f6a867a885df511aeb2409b604c64
Ludwig  Bauer <ludwigbauer@MacBook-Air-von-Ludwig.local>
2026-09-25 16:03:03 +0100

    Replace search emoji with Bootstrap Icons search SVG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -83,9 +83,17 @@ export default function Header({
 
         <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
           <div className="relative lg:w-3/5">
-            <span className="pointer-events-none absolute left-3.5 top-1/2 -translate-y-1/2 text-lg text-kraft-600">
-              🔍
-            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              className="pointer-events-none absolute left-3.5 top-1/2 h-4 w-4 -translate-y-1/2 text-kraft-600"
+              aria-hidden="true"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
             <input
               type="text"
               value={search}
