a6384fcbfab7118bac548ee0ce37f41db6101587
Ludwig  Bauer <ludwigbauer@MacBook-Air-von-Ludwig.local>
2026-09-25 16:40:56 +0100

    Replace language switcher icon with translate SVG

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import languageIcon from "../assets/icons/language.png";
 import { LOCALES } from "../i18n/translations";
 import { useLanguage } from "../i18n/LanguageContext";
 
@@ -31,7 +30,17 @@ export default function LanguageSwitcher() {
         aria-expanded={open}
         className="flex h-11 items-center justify-center rounded-md border border-kraft-300 bg-kraft-50 px-3 text-base font-medium text-kraft-800 hover:bg-kraft-200"
       >
-        <img src={languageIcon} alt="" className="h-6 w-6" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          aria-hidden="true"
+        >
+          <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286zm1.634-.736L5.5 3.956h-.049l-.679 2.022z" />
+          <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zm7.138 9.995q.289.451.63.846c-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6 6 0 0 1-.415-.492 2 2 0 0 1-.94.31" />
+        </svg>
       </button>
 
       {open && (
