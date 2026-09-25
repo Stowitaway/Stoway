deb629a6656ecb3fbfaabd2face2a0b6743b6faf
Ludwig  Bauer <ludwigbauer@MacBook-Air-von-Ludwig.local>
2026-09-25 16:35:26 +0100

    Replace hamburger character with sliders SVG icon in account menu

--- src/components/AccountMenu.jsx
+++ src/components/AccountMenu.jsx
@@ -26,9 +26,21 @@ export default function AccountMenu() {
         aria-haspopup="menu"
         aria-expanded={open}
         aria-label="Account menu"
-        className="flex h-11 w-11 items-center justify-center rounded-md border border-kraft-300 bg-kraft-50 text-lg text-kraft-800 hover:bg-kraft-200"
+        className="flex h-11 w-11 items-center justify-center rounded-md border border-kraft-300 bg-kraft-50 text-kraft-800 hover:bg-kraft-200"
       >
-        ☰
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1z"
+          />
+        </svg>
       </button>
 
       {open && (
