049cde74709306a9f7d7770cd83e2ab07624a48b
Ludwig  Bauer <ludwigbauer@MacBook-Air-von-Ludwig.local>
2026-09-25 16:29:20 +0100

    Switch closet icon to outline door-closed SVG

--- src/components/RoomTypeIcon.jsx
+++ src/components/RoomTypeIcon.jsx
@@ -21,7 +21,8 @@ function DoorIcon({ className }) {
       className={className}
       aria-hidden="true"
     >
-      <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+      <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3zm1 13h8V2H4z" />
+      <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0" />
     </svg>
   );
 }
